41fd38e92f5cb4f64efde1ce0967bd81626082f6
Hal Mueller <hal@mobilegeographics.com>
2015-10-24 15:20:21 -0700

    Add a third sprite, to show user acceleration.

--- Plummet/Plummet/PlummetScene.swift
+++ Plummet/Plummet/PlummetScene.swift
@@ -12,6 +12,7 @@ import CoreMotion
 class PlummetScene: SKScene {
     var markerSprite: SKShapeNode!
     var centerPoint: SKShapeNode!
+	var accelerationSprite: SKShapeNode!
     
     var motionManager: CMMotionManager!
     
@@ -37,27 +38,38 @@ class PlummetScene: SKScene {
         centerPoint.fillColor = SKColor.yellowColor()
         
         markerSprite = SKShapeNode(circleOfRadius: 5.0)
-        markerSprite?.fillColor = SKColor.redColor()
-
+        markerSprite.fillColor = SKColor.redColor()
+		
+		accelerationSprite = SKShapeNode(rectOfSize: CGSizeMake(8.0, 8.0))
+		accelerationSprite.fillColor = SKColor.greenColor()
+		accelerationSprite.strokeColor = SKColor.greenColor()
+		
+		self.addChild(accelerationSprite)
         self.addChild(markerSprite)
         self.addChild(centerPoint)
     }
     
     override func didFinishUpdate() {
-        if let motion = motionManager.deviceMotion {
-            //			let attitude = motion.attitude
-            //			print ("attitude", attitude.pitch, attitude.roll, attitude.yaw)
-            let gravity = motion.gravity
-//            print ("gravity", gravity.x, gravity.y, gravity.z)
-            
-            let gravityMultiple = 100.0
-            let xDelta = CGFloat(gravity.x * gravityMultiple)
-            let yDelta = CGFloat(gravity.y * gravityMultiple)
-            markerSprite.position = CGPointMake(centerPoint.position.x + xDelta,
-                centerPoint.position.y + yDelta)
-        }
     }
 
     override func update (currentTime: NSTimeInterval) {
+		if let motion = motionManager.deviceMotion {
+			//			let attitude = motion.attitude
+			//			print ("attitude", attitude.pitch, attitude.roll, attitude.yaw)
+			let gravity = motion.gravity
+			//            print ("gravity", gravity.x, gravity.y, gravity.z)
+			
+			let gravityMultiple = 100.0
+			let xDelta = CGFloat(gravity.x * gravityMultiple)
+			let yDelta = CGFloat(gravity.y * gravityMultiple)
+			markerSprite.position = CGPointMake(centerPoint.position.x + xDelta,
+				centerPoint.position.y + yDelta)
+			
+			let acceleration = motion.userAcceleration
+			let xAccelDelta = CGFloat(acceleration.x * gravityMultiple)
+			let yAccelDelta = CGFloat(acceleration.y * gravityMultiple)
+			accelerationSprite.position = CGPointMake(centerPoint.position.x + xAccelDelta,
+				centerPoint.position.y + yAccelDelta)
+		}
     }
 }
