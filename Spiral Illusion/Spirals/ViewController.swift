d3d5e34e17b2d7316c52455d14663845396c2646
Hal Mueller <hal@mobilegeographics.com>
2015-10-26 23:07:02 -0700

    Reorganize to split out shareable files, and add tvOS target.

---
+++ Spiral Illusion/Spirals/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Spirals
+//
+//  Created by Hal Mueller on 10/26/15.
+//  Copyright © 2015 Hal Mueller. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var spriteKitView: SKView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let illusionScene = SpiralIllusionScene(size: spriteKitView.bounds.size)
+		spriteKitView.showsFPS = false
+		spriteKitView.presentScene(illusionScene)
+		spriteKitView.backgroundColor = illusionScene.backgroundColor
+		view.backgroundColor = illusionScene.backgroundColor
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
+
