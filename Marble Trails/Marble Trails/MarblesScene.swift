d9ce2ab74dfcc942ba2a80b7ca2558b41d6fc34d
Hal Mueller <halmueller@gmail.com>
2015-11-02 19:51:32 -0800

    tilt-controlled marbles demo

--- Marble Trails/Marble Trails/MarblesScene.swift
+++ Marble Trails/Marble Trails/MarblesScene.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2015 Hal Mueller. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+import CoreMotion
+
+class MarblesScene: SKScene {
+
+    var motionManager: CMMotionManager?
+
+    override init(size: CGSize) {
+        super.init(size: size)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func didMoveToView(view: SKView) {
+        motionManager = CMMotionManager()
+        motionManager?.startDeviceMotionUpdatesUsingReferenceFrame(.XTrueNorthZVertical)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+    }
+
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+
+            let sprite = SKShapeNode(circleOfRadius: 10.0)
+            sprite.physicsBody = SKPhysicsBody(circleOfRadius: 10.0)
+            sprite.physicsBody?.affectedByGravity = true
+            sprite.physicsBody?.dynamic = true
+            sprite.fillColor = SKColor.yellowColor()
+            sprite.position = location
+            self.addChild(sprite)
+        }
+    }
+
+    override func update (currentTime: NSTimeInterval) {
+        if let motion = motionManager?.deviceMotion {
+            let gravity = motion.gravity
+            //			let attitude = motion.attitude
+            //			print ("attitude", attitude.pitch, attitude.roll, attitude.yaw)
+            let adjustment = 1.0
+            self.physicsWorld.gravity.dx = CGFloat(adjustment * gravity.x)
+            self.physicsWorld.gravity.dy = CGFloat(adjustment * gravity.y)
+        }
+    }
+}
--- Marble Trails/Marble Trails/ViewController.swift
+++ Marble Trails/Marble Trails/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import SpriteKit
+
+extension SKNode {
+
+    class func unarchiveFromFile<SceneSubclass:SKScene>(file : String) -> SceneSubclass? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            let sceneData = try! NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
+            let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! SceneSubclass
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skView: SKView!
+
+    let sceneNames = ["BlocksSceneOne", "BlocksSceneTwo", "MarblesScene", "MarblesSceneTwo"]
+    var sceneIndex = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.showsDrawCount = true
+        if let scene = MarblesScene.unarchiveFromFile(sceneNames[sceneIndex++]) as? MarblesScene {
+            scene.scaleMode = .AspectFit
+            skView.presentScene(scene)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func changeScene(sender: AnyObject) {
+        let sceneName = sceneNames[sceneIndex]
 
+        if ++sceneIndex == sceneNames.count {
+            sceneIndex = 0
+        }
 
+        if let scene = MarblesScene.unarchiveFromFile(sceneName) as? MarblesScene {
+            scene.scaleMode = .AspectFit
+            skView.presentScene(scene)
+        }
+    }
+    
 }
 
