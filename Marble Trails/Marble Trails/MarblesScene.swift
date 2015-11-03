080d5a3ea1b99927b00892b8568ddd4f20882ee2
Hal Mueller <halmueller@gmail.com>
2015-11-02 19:50:53 -0800

    tilt-controlled marbles demo

---
+++ Marble Trails/Marble Trails/MarblesScene.swift
@@ -0,0 +1,9 @@
+//
+//  MarblesScene.swift
+//  Marble Trails
+//
+//  Created by Hal Mueller on 11/2/15.
+//  Copyright © 2015 Hal Mueller. All rights reserved.
+//
+
+import Foundation
---
+++ Marble Trails/Marble Trails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marble Trails
+//
+//  Created by Hal Mueller on 11/2/15.
+//  Copyright © 2015 Hal Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
