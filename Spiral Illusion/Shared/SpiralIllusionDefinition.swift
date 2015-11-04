468dfd088249c0262c6bf32eabce99f6a95a3d03
Hal Mueller <hal@mobilegeographics.com>
2015-11-03 19:51:36 -0800

    I like these rotation parameters better.

--- Spiral Illusion/Shared/SpiralIllusionDefinition.swift
+++ Spiral Illusion/Shared/SpiralIllusionDefinition.swift
@@ -9,12 +9,12 @@
 import SpriteKit
 
 struct SpiralIllusionDefinition {
-    let numberOfRings = 4
+    let numberOfRings = 6
 	let squareSize = 16.0
 	let squareSeparation = 6.0
-	let innerRingRotationRate = 0.2
-	let ringRotationRateIncrement = -0.1
-	let rotationDuration = 7.0
+	let innerRingRotationRate = 0.25 * 10 * M_PI
+	let ringRotationRateIncrement = -0.1 * 10 * M_PI
+	let rotationDuration = 20.0
 	let oddColor = SKColor(white: 0.88, alpha: 1.0)
 	let evenColor = SKColor(white: 0.0, alpha: 1.0)
 	let backgroundColor = SKColor(white: 0.46, alpha: 1.0)
