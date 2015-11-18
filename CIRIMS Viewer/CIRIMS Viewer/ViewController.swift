83681a22d04cd544e96da088e6c7f4c42793b797
Hal Mueller <hal@mobilegeographics.com>
2015-11-17 21:30:17 -0800

    try! means never having to say you'e sorry.

--- CIRIMS Viewer/CIRIMS Viewer/ViewController.swift
+++ CIRIMS Viewer/CIRIMS Viewer/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
 
 	@IBOutlet weak var subView: UIView!
 	@IBOutlet weak var webView: WKWebView?
+	var CIRIMSString: String?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		let webViewConfiguration = WKWebViewConfiguration()
@@ -35,7 +37,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
 		]
 		NSLayoutConstraint.activateConstraints(constraints)
 		
-		newWebView.loadRequest(NSURLRequest(URL: NSURL(string: "https://www.apple.com")!))
+		loadAndViewCIRIMSFileAtURLPath("http://cirims.apl.washington.edu/DataFiles/CIRIMS04_Guam_Japan_v1.0.txt")
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -43,6 +45,30 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	// CIRIMS data files: http://cirims.apl.washington.edu/DataFiles/
+	func loadAndViewCIRIMSFileAtURLPath(URLString: String) {
+		// load http://cirims.apl.washington.edu/DataFiles/CIRIMS04_Guam_Japan_v1.0.txt
+		print(URLString)
+		let dataString = try! String(contentsOfURL: NSURL(string: URLString)!)
+		print (dataString)
+	}
+	
+	// MARK: - WKNavigationDelegate
+	func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+		print ("loading", webView.URL?.path)
+	}
+	
+	func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!,
+		withError error: NSError) {
+			print("didFailProvisionalNavigation", navigation, error.localizedDescription, error.localizedFailureReason)
+			let alertController = UIAlertController(title:"Load failed", message: error.localizedDescription, preferredStyle: .Alert)
+			alertController.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+			self.presentViewController(alertController, animated: true) { }
+	}
+	
+	// MARK: - WKUIDelegate
+	func webView(webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: () -> Void) {
+		fatalError("not yet implemented")
+	}
 }
 
